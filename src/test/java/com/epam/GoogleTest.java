1fd7f15c23b40ebe85095cf2d6eb80355d0a0505
Yury Staravoitau <yury.starav@gmail.com>
2018-10-06 12:47:04 -0500

    Update test - add tags for the test

--- src/test/java/com/epam/GoogleTest.java
+++ src/test/java/com/epam/GoogleTest.java
@@ -4,11 +4,13 @@ import com.epam.steps.GoogleSteps;
 import com.epam.webdriver.WebDriverType;
 import io.qameta.allure.*;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 
+@Tag("all")
 @Epic("EPAM")
 @Feature("GOOGLE SEARCH")
 @Story("Validate EPAM in Google search results")
